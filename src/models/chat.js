23d5e57ad5a9c7a04a327b1da50b3b7bfcc9e728
Anisul Islam <anisulislam.pranto@gmail.com>
2024-12-18 22:47:19 +0600

    sender can be shop or user

--- src/models/chat.js
+++ src/models/chat.js
@@ -3,13 +3,21 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
   {
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "senderType", // Reference will depend on senderType
+    },
+    senderType: {
+      type: String,
+      required: true,
+      enum: ["User", "Shop"], // Could be either a User or Shop
+    },
     message: { type: String, required: true },
     timestamp: { type: Date, default: Date.now },
   },
   { _id: false }
 );
-
 const chatSchema = new mongoose.Schema(
   {
     shop: { type: mongoose.Schema.Types.ObjectId, ref: "Shop", required: true },
